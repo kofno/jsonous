27977dd5804bd8d182c8ed4c74ff530ef63640e7
Ryan L. Bell <ryan.l.bell@gmail.com>
2017-09-29 23:41:15 -0400

    chore: remove generated code (dist dir)
    Since we're releasing from travis-ci now, I don't want generated code in checked into the repo anymore.

--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-import Decoder, { array, at, boolean, date, fail, field, maybe, number, oneOf, string, succeed } from './Decoder';
-export default Decoder;
-export { array, at, boolean, date, fail, field, maybe, number, oneOf, string, succeed };
